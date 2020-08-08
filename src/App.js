201acfe8d3c042386799e1cbb82e6bf068ba0a8f
Yokeshwaran <callmeyokesh@>
2020-08-08 10:55:45 +0530

    Changes in commands text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Custom <code>commands</code> for MPX console.
         </p>
         <a
           className="App-link"
